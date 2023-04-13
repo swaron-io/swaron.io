9bb913c9fde5e36e52b30af2490b657f4b923227
lxcvz <lucas-mateus.dc@hotmail.com>
2023-04-13 18:12:33 -0300

    added comming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={`${inter.className}`}>
-      <Header />
-      <div className="grid grid-cols-2 h-[750px]">
-        <div className="grid-col border"></div>
-        <div className="grid-col border"></div>
+      <div className="h-screen flex justify-center items-center flex-col">
+        <h3 className="text-2xl font-bold">
+          swar<span className="text-blue">o</span>n.io
+        </h3>
+        <h1 className="text-lg font-bold text-high_gray">coming soon.</h1>
       </div>
     </main>
   );
