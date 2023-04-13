dace6db87a40b43dbd04d12f381203ed0cc65c60
anddreluis2 <anddreluis98@gmail.com>
2023-04-12 22:28:56 -0300

    added colors to tailwind

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="flex justify-between px-24 py-8">
+      <a>about</a>
+      <h3>swaron</h3>
+      <button className="">login</button>
+    </header>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      neutralgray: "#E5E5E5",
+      text: "#262626",
+      blue: "#2563EB",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
