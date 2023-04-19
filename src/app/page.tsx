205279fc9abec2b30e27c0b9ca7c3ebbc2d93e14
lxcvz <lucas-mateus.dc@hotmail.com>
2023-04-18 21:03:32 -0300

    feat: added title and subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,21 @@ export default function Home() {
   return (
     <main className={`${inter.className}`}>
       <Header />
-      <div className="grid grid-cols-2 h-[750px]">
-        <div className="flex justify-end pr-4 items-center">
+      <section className="grid grid-cols-2 py-14 px-20 border border-black">
+        <div className="flex flex-col">
+          <div>
+            <h1 className="text-6xl/tight font-bold">Learn IT for free</h1>
+            <h2 className="text-5xl font-bold ">with swaron</h2>
+          </div>
+          <span className="py-6">
+            Swaron IO is a platform to learn for free. We believe that knowledge
+            must be accessible to everyone.
+          </span>
+        </div>
+        <div className="flex justify-end pl-4 items-center">
           aqui ficara uma imagem
         </div>
-        <div className="flex justify-start pl-4 items-center">
-          aqui ficara o texto
-        </div>
-      </div>
+      </section>
     </main>
   );
 }
