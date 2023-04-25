e5c0e25a41976a1b864ba53fea685afe97153b8a
murillo.nahas <murillonahas@hotmail.com>
2023-04-25 13:09:16 -0300

    feat: commit

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,17 @@
-export function Footer() {
-  return <footer>oii</footer>;
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>© 2023 swaron.</p>
+        <p>Terms of Service • Privacy policy</p>
+      </div>
+      <nav>
+        <div>Twitter</div>
+        <div>Insta</div>
+        <div>Git</div>
+        <div>Yt</div>
+        <div>Twitch</div>
+      </nav>
+    </footer>
+  );
 }
