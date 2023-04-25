5b6248e742e3f9e51a841f1119e77af810fbc489
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-04-25 12:41:23 -0300

    feat: metatags seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "./globals.css";
 
 export const metadata = {
@@ -12,6 +13,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Social Media Preview</title>
+        <meta property="og:url" content="https://swaron.io" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="swaron.io" />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="SwaronIO is a platform to learn computer science for free. Join our waitlist to get early access."
+        />
+        <meta property="og:image" content='https://cdn.discordapp.com/attachments/1088918960533164106/1100446169542434961/3b2c3981-1dad-4bb2-8413-2850921432f7.png' />
+      </Head>
       <body
         className="bg-app_bg"
       >
