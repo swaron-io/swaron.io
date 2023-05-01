21cfe35fc908dafe7f1cd1e89c5fd56189bba1cc
anddreluis2 <anddreluis98@gmail.com>
2023-05-01 16:52:32 -0300

    added our team button

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,27 +3,35 @@ import { Dropdown } from "./dropdown";
 
 export function Header() {
   return (
-    <header className="mx-auto flex w-[95%] max-w-[1140px] items-center justify-start py-8">
-      <Link href={"/"}>
-        <h3 className="text-2xl font-bold">
-          swar<span className="text-blue">o</span>n
-        </h3>
-      </Link>
-      <span className="hidden items-center gap-2 sm:flex">
-        <h3 className="ml-4 text-high_gray md:ml-24">Learning paths</h3>
-        <Dropdown />
-      </span>
-      <Link className="ml-4 mr-auto text-high_gray md:ml-24" href={"/about"}>
-        <h3>About</h3>
-      </Link>{" "}
-      {/* <Link className=" mr-auto text-high_gray md:ml-24" href={"/team"}>
-        <h3>our team</h3>
-      </Link> */}
-      <Link href="/#email">
-        <button className="rounded-full bg-blue px-8 py-2 font-semibold text-white duration-200 hover:bg-light_blue">
-          Join
-        </button>
-      </Link>
+    <header className="mx-auto flex w-[95%] max-w-[1140px] items-center py-16">
+      <div className="flex w-[95%] justify-between">
+        <Link href={"/"}>
+          <h3 className="items-center text-2xl font-bold">
+            swar<span className="text-blue">o</span>n
+          </h3>
+        </Link>
+        <span className="hidden items-center gap-2 sm:flex">
+          <h3 className="ml-4 text-high_gray md:ml-24">Learning paths</h3>
+          <Dropdown />
+        </span>
+        <Link
+          className="ml-4 mr-auto flex items-center text-high_gray md:ml-24"
+          href={"/team"}
+        >
+          <h3>Our team</h3>
+        </Link>
+        <Link
+          className="ml-4 mr-auto flex items-center text-high_gray md:ml-[-200px]"
+          href={"/about"}
+        >
+          <h3>About</h3>
+        </Link>{" "}
+        <Link href="/#email">
+          <button className="rounded-full bg-blue px-8 py-2 font-semibold text-white duration-200 hover:bg-light_blue">
+            Join
+          </button>
+        </Link>
+      </div>
     </header>
   );
 }
